60b60c6b1aee8d80f04ee20bdc1941b6e1139c55
Iain J McCallum <imccallum@delphicdigital.com>
2016-11-23 14:01:36 -0500

    beginning to set up babel

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
-import { polyfill } from 'es6-promise';
-import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM     from 'react-dom';
-import ZipCode from './components/ZipCode/ZipCode.js';
-import ChannelTable from './components/ChannelTable/ChannelTable.js';
 
-//console.info(`React js version : ${React.version}`);
 
-//Testing pass data between separate components with PubSub
 
 ReactDOM.render(
-	<ZipCode zipLocateURL="https://maps.googleapis.com/maps/api/geocode/json"/>,
-	document.getElementById('zip-form')
-);
-
-ReactDOM.render(
-	<ChannelTable channelsURL="http://www.mocky.io/v2/57fbea5d0f000060184fd487"/>,
-	document.getElementById('main')
+	<h1>Hello!</h1>,
+	document.getElementById('app')
 );
